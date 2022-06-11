f2a090ed8615fca7945e9300c7f18bd8b8f4169b
Robin Braumann <robin.braumann@gmail.com>
2022-06-11 16:44:35 +0200

    remove google oauth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDTO } from 'src/dtos/authDTO';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  createUser(@Body() authDTO: AuthDTO): string {
+    return this.authService.createUser();
+  }
+}
