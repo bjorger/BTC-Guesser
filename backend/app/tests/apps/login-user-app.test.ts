e042ffbaf6e28997b5eaeecc325ec4f7a2498ea1
Robin Braumann <robin.braumann@gmail.com>
2022-06-13 03:24:57 +0200

    start with login test

--- backend/app/tests/apps/login-user-app.test.ts
+++ backend/app/tests/apps/login-user-app.test.ts
@@ -1,11 +1,27 @@
-import "mocha";
 import { expect } from "chai";
-import { Mock, Times } from "moq.ts";
-import { ApiGatewayResponse } from "../../src/common/apigateway/apigateway-response";
-import { ApiGatewayEventMock } from "../mocks/apigateway-event-mock";
-import { UserState, UserResponse, UserRepository } from "../../src/common/user";
-import { CreateUserApp } from "../../src/apps/create-user-app";
+import "mocha";
+import { Mock } from "moq.ts";
+import { LoginUserApp } from "../../src/apps/login-user-app";
+import { UserRepository, UserResponse, UserState } from "../../src/common/user";
 
 describe("UserLogin instance", () => {
-    
+    const repoMock = new Mock<UserRepository>()
+        .setup((instance) => instance.createUser("Robin_Braumann", "Hallo123"))
+        .returns(
+            new Promise<UserResponse>((resolve) => {
+                resolve({
+                    username: "Robin_Braumann",
+                    state: UserState.CAN_GUESS,
+                    score: 0,
+                });
+            }),
+        );
+
+    describe("constructor", () => {
+        it("repository is assigned", () => {
+            const app = new LoginUserApp(repoMock.object());
+
+            expect(app.repository).to.equal(repoMock.object());
+        });
+    });
 });
