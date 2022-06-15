15677a7b5ffe9e9acf2102d88b49abb8a7697461
Robin Braumann <robin.braumann@gmail.com>
2022-06-15 15:47:38 +0200

    change user hint text

--- frontend/src/common/globals.ts
+++ frontend/src/common/globals.ts
@@ -1,7 +1,6 @@
 export const AWSEndpoint = process.env.REACT_APP_AWS_ENDPOINT || "";
 export const JWTCookieName = process.env["REACT_APP_JWT_COOKIE_NAME"] || "";
-export const USER_HINT_START =
-    "To make a guess of the Bitcoin trend hit either UP or DOWN. After your guess has been placed, it will be evaluated in the next 60 seconds.";
+export const USER_HINT_START = "To make a guess of the Bitcoin trend click either UP or DOWN. Your guess will be evaluated in the next 60 seconds.";
 export const USER_HINT_GUESSING =
     "Your guess has been accepted! This page will update itself and inform you if your guess was either successful or not.";
 export const USER_HINT_GUESS_SUCCESS = "Your guess was a success! Check your score to see how many points you have.";
